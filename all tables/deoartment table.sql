10454bf56c57b9e940770ddb41b46406e5e3571c
vaibhav mane <manevaibhav6353@gmail.com>
2021-05-11 16:39:52 +0530

    department table created

---
+++ all tables/deoartment table.sql
@@ -0,0 +1,11 @@
+
+-----------------------------------------------
+-- CREATED BY AISHWARYA MANE
+-----------------------------------------------
+
+
+CREATE TABLE DEPARTMENT
+(	
+	D_ID INT
+	,D_NAME VARCHAR(50)
+)
