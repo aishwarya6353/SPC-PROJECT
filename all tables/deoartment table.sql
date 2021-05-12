ff825bd8f7914630c92cb0a6504c74f55b0aaa47
aishwarya mane <maneaishwarya6353@gmail.com>
2021-05-12 12:09:18 +0530

    dummy record added in department table fix 07

--- all tables/deoartment table.sql
+++ all tables/deoartment table.sql
@@ -4,9 +4,44 @@
 -----------------------------------------------
 
 
+
+
+
+/*
+-----------------------------------------------------------
+ISSUE NAME		= #8 ADD SOME DUMMY RECORDS IN DEPARTMENT TABLE
+CREATED BY		= AISHWARYA MANE
+CALLED BY		= VAIBHAV MANE
+DESCRICPTION	= ADDING SOME DUMMY RECORDS IN DEPARTMETN TABLE
+-----------------------------------------------------------
+CHANGED BY			APPROVED BY 
+AISHWARYA MANE		VAIBHAV MANE
+-----------------------------------------------------------
+
+*/
+
 CREATE TABLE DEPARTMENT
 (	
 	D_ID INT
 	,D_NAME VARCHAR(50)
-	,d_fname varchar(50)
 )
+
+INSERT INTO DEPARTMENT 
+	VALUES
+		(
+			10
+			,'HR'
+		)
+		,
+		(
+			20
+			,'SQL'
+		)
+		,
+		(
+			30
+			,'FINANCE'
+		)
+
+-------------------------------------------------------------
+
