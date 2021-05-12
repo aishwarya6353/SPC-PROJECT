d7d1915d84648f0246b74cae21ba72c1d42836d6
aishwarya mane <maneaishwarya6353@gmail.com>
2021-05-12 11:04:26 +0530

    dummy records added

--- all tables/Emp table added.sql
+++ all tables/Emp table added.sql
@@ -1,5 +1,15 @@
+/*
+-----------------------------------------------------------
+ISSUE NAME		= #8 ADD SOME DUMMY RECORDS IN EMPLOYEE TABLE
+CREATED BY		= AISHWARYA MANE
+CALLED BY		= VAIBHAV MANE
+DESCRICPTION	= ADDING SOME DUMMY RECORDS IN EMPLOYEE TABLE
+-----------------------------------------------------------
+CHANGED BY			APPROVED BY 
+AISHWARYA MANE		VAIBHAV MANE
+-----------------------------------------------------------
 
-USE SPC
+*/
 
 CREATE TABLE EMPLOYEE
 (	
@@ -9,3 +19,32 @@ CREATE TABLE EMPLOYEE
 	,E_DOB DATE
 	,D_ID INT
 )
+
+INSERT INTO EMPLOYEE 
+	VALUES
+		(
+			1
+			,'VAIBHAV'
+			, 'MANE'
+			,'1993-08-09'
+			,10
+		)
+		,
+		(
+			2
+			,'VISHAL'
+			, 'MANE'
+			,'1997-06-27'
+			,20
+		)
+		,
+		(
+			3
+			,'PANKAJ'
+			, 'MANE'
+			,'1997-07-25'
+			,30
+		)
+
+-------------------------------------------------------------
+
